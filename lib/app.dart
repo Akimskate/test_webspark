ed3df58a8a4307d979a0f3a3ef40e65211571f03
Akim Svoboda <akim.svoboda@gmail.com>
2024-02-27 03:03:21 +0200

    home page completed

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:test_webspark/home/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Webspark test task',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
